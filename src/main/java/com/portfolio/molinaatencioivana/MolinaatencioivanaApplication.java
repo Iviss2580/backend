2ac37ecf8a4766cd3c9859e26883d36a2e11a46c
Iviss2580 <iviss036@hotmail.com>
2022-07-14 12:20:45 -0300

    primera parte back end

---
+++ src/main/java/com/portfolio/molinaatencioivana/MolinaatencioivanaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.molinaatencioivana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolinaatencioivanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolinaatencioivanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/molinaatencioivana/MolinaatencioivanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.molinaatencioivana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MolinaatencioivanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
