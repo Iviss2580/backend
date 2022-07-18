ac866e1bca6776fb23503193c3ad4bb5c60a4355
Iviss2580 <iviss036@hotmail.com>
2022-07-18 11:37:52 -0300

    inicializar el repositorio de back

--- src/main/java/com/portfolio/molinaatencioivana/Controller/PersonaController.java
+++ src/main/java/com/portfolio/molinaatencioivana/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.molinaatencioivana.Entity.Persona;
 import com.portfolio.molinaatencioivana.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -45,6 +47,8 @@ public class PersonaController {
         return persona;
     }
     @GetMapping("/personas/traer/perfil")
-    public Persona findpersona(){return iPersonaService.findPersona((long)1);
+    public Persona findpersona(){
+        return iPersonaService.findPersona((long)1);
+    }
     
 }
