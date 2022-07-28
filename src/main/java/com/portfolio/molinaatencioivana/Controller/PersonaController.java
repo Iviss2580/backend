48e429f5bfb4e204dfabc8d88e8c91692861b1e0
Iviss2580 <iviss036@hotmail.com>
2022-07-28 17:34:13 -0300

    EDUCACION

--- src/main/java/com/portfolio/molinaatencioivana/Controller/PersonaController.java
+++ src/main/java/com/portfolio/molinaatencioivana/Controller/PersonaController.java
@@ -52,7 +52,7 @@ public class PersonaController {
     }
     @GetMapping("/personas/traer/perfil")
     public Persona findpersona(){
-        return iPersonaService.findPersona((long)1);
+        return ipersonaService.findPersona((long)1);
     }
     
 }
